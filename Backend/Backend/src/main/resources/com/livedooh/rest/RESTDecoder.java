185c0add778635c105b39a2312380c1781679704
Kai <kai.fichtner@googlemail.com>
2015-12-10 22:58:29 +0100

    ADD: REST pathes

--- Backend/Backend/src/main/resources/com/livedooh/rest/RESTDecoder.java
+++ Backend/Backend/src/main/resources/com/livedooh/rest/RESTDecoder.java
@@ -1,5 +1,17 @@
 package com.livedooh.rest;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 public class RESTDecoder {
 
+	@PATH("/decoder")
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayPlainTextHello() {
+		return "Hello Jersey";
+	}
+	
 }
