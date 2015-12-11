2b2251b6ad26ad624cbcf656826e088e06cbfa1d
Kai <kai.fichtner@googlemail.com>
2015-12-11 14:57:36 +0100

    UPDATE: Dependencys

--- Backend/Backend/src/main/java/com/livedooh/rest/RESTDecoder.java
+++ Backend/Backend/src/main/java/com/livedooh/rest/RESTDecoder.java
@@ -5,12 +5,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@PATH("/decoder")
+import org.json.JSONObject;
+
+@Path("/decoder")
 public class RESTDecoder {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public String decodeString(JSONObject json) {
+		return null;
 		
 	}
 	
--- Backend/Backend/src/main/java/com/livedooh/rest/RESTEncoder.java
+++ Backend/Backend/src/main/java/com/livedooh/rest/RESTEncoder.java
@@ -4,13 +4,15 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.json.JSONObject;
 
-@PATH("/encoder")
+@Path("/encoder")
 public class RESTEncoder {
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public String encodeString(JSONObject json) {
+		return null;
 		
 	}
 
