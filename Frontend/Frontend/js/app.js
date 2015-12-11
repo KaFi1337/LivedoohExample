a8f290ddd2f7fa075e08e2eca688dd7c3536b908
Kai <kai.fichtner@googlemail.com>
2015-12-11 19:19:56 +0100

    ADD: Encode/Decode Controller

--- Frontend/Frontend/js/app.js
+++ Frontend/Frontend/js/app.js
@@ -1,7 +1,7 @@
 (function() {
 	var app = angular.module('LivedoohExample', []);
 
-	app.controller('TabController', function() {
+	app.controller('TabCtrl', function() {
 		this.tab = 1;
 
 		this.setTab = function(newValue) {
@@ -13,6 +13,46 @@
 		};
 	});
 
-	app.controller()
+	app.controller('DecodeCtrl', function($http, $scope) {
+
+		this.getResult = function(code) {
+
+			$http({
+				url : "",
+				method : 'GET',
+				data : code,
+				header : {
+					"Content-Type" : "application/json; charset=utf-8"
+				}
+			}).success(function(response) {
+				$scope.decode = response;
+			}).error(function(response) {
+				console.log(response);
+			});
+
+		}
+
+	});
+	
+	app.controller('EncodeCtrl', function($http, $scope) {
+
+		this.getResult = function(code) {
+
+			$http({
+				url : "",
+				method : 'GET',
+				data : code,
+				header : {
+					"Content-Type" : "application/json; charset=utf-8"
+				}
+			}).success(function(response) {
+				$scope.encode = response;
+			}).error(function(response) {
+				console.log(response);
+			});
+
+		}
+
+	});
 
 })();
